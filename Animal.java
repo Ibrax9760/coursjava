fc663aa8a2b01151fd82742f418e341981668537
ALI Ibrahim <aliibrahim.ihm@gmail.com>
2025-06-13 17:38:49 +0200

    update du fichier pour le rendre pour le rendre abstraite

--- Animal.java
+++ Animal.java
@@ -1,15 +1,11 @@
- public class Animal {
+public abstract class Animal {
     protected String nom;
     protected int age;
+
     public Animal(String nom, int age) {
         this.nom = nom;
         this.age = age;
     }
-    public void faireDuBruit() {
-        System.out.println("L'animal fait un bruit.");
-    }
 
-    public static void main(String[] args ) {
-        
-    }
- }
+    public abstract void faireDuBruit();
+}
--- TestAnimaux.java
+++ TestAnimaux.java
@@ -1,7 +1,7 @@
 public class TestAnimaux {
     public static void main(String[] args) {
         Animal[] animaux = new Animal[3];
-        animaux[0] = new Animal("Bête", 3);
+        animaux[0] = new Chien("Bobby", 4);
         animaux[1] = new Chien("Rex", 5);
         animaux[2] = new Chat("Mimi", 2);
 
