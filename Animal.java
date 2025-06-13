6b410e505bdae0317687802a98c58efa74744115
ALI Ibrahim <aliibrahim.ihm@gmail.com>
2025-06-13 13:00:09 +0200

    Ajout du fichier Animal.java

---
+++ Animal.java
@@ -0,0 +1,15 @@
+ public class Animal {
+    protected String nom;
+    protected int age;
+    public Animal(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+    public void faireDuBruit() {
+        System.out.println("L'animal fait un bruit.");
+    }
+
+    public static void main(String[] args ) {
+        
+    }
+ }
